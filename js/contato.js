574d21633ae107b893884d375f127cc4bc573016
Eduardo Carreiro <eduardo@wpcwebsolutions.com>
2022-03-19 17:26:05 -0300

    adding testimonials in contact.html

--- js/contato.js
+++ js/contato.js
@@ -20,3 +20,59 @@ inputTel.style.boxSizing = "border-box";
 let btnContato = document.querySelector("#enviar");
 // console.log(btnContato);
 btnContato.style.width = "150px";
+
+//3.5 Abaixo da section do formulário, adicione uma seção de comentário igual a página Home.
+let comments = `<section class="titulo depoimento">
+				<h3>Depoimentos</h3>
+			</section><section id="falam_sobre">
+				<div class="depoimentos">
+					<img src="./imagens/icon-wally.png" width="80px" height="80px" alt="depoimentos da dindim" />
+					<p>
+						”Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+						laudantium, totam rem aperiam.”
+						<br />
+						<br />
+						Wally, 25
+					</p>
+				</div>
+
+				<div class="depoimentos">
+					<img src="./imagens/icon-jaden.png" width="80px" height="80px" alt="depoimentos da dindim" />
+					<p>
+						”Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+						laudantium, totam rem aperiam.”
+						<br />
+						<br />
+						Jaden Smith, 23
+					</p>
+				</div>
+
+				<div class="depoimentos">
+					<img src="./imagens/icon-whoopi.png" width="80px" height="80px" alt="depoimentos da dindim" />
+					<p>
+						”Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+						laudantium, totam rem aperiam.”
+						<br />
+						<br />
+						Whoopi Goldberg, 37
+					</p>
+				</div>
+
+				<div class="depoimentos">
+					<img src="./imagens/icon-jane.png" width="80px" height="80px" alt="depoimentos da dindim" />
+					<p>
+						”Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+						laudantium, totam rem aperiam.”
+						<br />
+						<br />
+						Janes Joplin, 29
+					</p>
+				</div>
+			</section>`;
+
+let addComments = document.querySelector("main");
+addComments.innerHTML += comments;
+
+document.querySelector(".formulario").style.minHeight = "30px";
+
+document.querySelector(".titulo.depoimento").style.margin = "30px auto";
